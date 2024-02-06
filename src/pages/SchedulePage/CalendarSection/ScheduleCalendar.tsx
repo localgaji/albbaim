11607bc1719a5688bd29b85a663680372d7d883a
localgaji <oorongcha@naver.com>
2024-02-07 03:46:04 +0900

    refactor: props 전달 구조 변경

--- src/pages/SchedulePage/CalendarSection/ScheduleCalendar.tsx
+++ src/pages/SchedulePage/CalendarSection/ScheduleCalendar.tsx
@@ -1,7 +1,7 @@
-import { DailyWorkTimeData } from 'apis/types';
 import { Badge, BadgeCont, BadgeText, MonthBox, WeekGrid } from 'components/Calendar/CalendarStyle';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { useGetMonthly } from 'pages/SchedulePage/hooks/fetch';
+import { DailySchedule } from 'types/schedule';
 import { dateAtom, monthAtom } from '../states';
 import CalendarDayBox from './CalendarDayBox';
 
@@ -14,19 +14,18 @@ const ScheduleCalendar = (): JSX.Element => {
 
   return (
     <MonthBox $wFull data-testid="월간스케줄">
-      {scheduleData?.table.map((weekArray: DailyWorkTimeData[], i) => (
+      {scheduleData?.monthly.map((weekArray: DailySchedule[], i) => (
         <WeekGrid key={`${i}주`} $aspectRatio="5.6">
-          {weekArray.map((e: DailyWorkTimeData) => (
+          {weekArray.map((dailyData: DailySchedule) => (
             <CalendarDayBox
-              key={e.date}
-              dateString={e.date}
-              timeList={e.workTime}
-              onClick={() => setSelectedDate({ isFixed: e.workTime !== null, date: e.date })}
-              isSelected={selectedDate.date === e.date}
+              key={dailyData.date}
+              dailyData={dailyData}
+              onClick={() => setSelectedDate({ hasFixed: dailyData.hasFixed, date: dailyData.date })}
+              isSelected={selectedDate.date === dailyData.date}
             >
               <BadgeCont>
-                {e.workTime?.map((title) => (
-                  <Badge key={title} $color={scheduleData.badgeColor[title]}>
+                {dailyData.workTimes?.map((title) => (
+                  <Badge key={title} $color="#DEFFE1">
                     <BadgeText>{title}</BadgeText>
                   </Badge>
                 ))}
